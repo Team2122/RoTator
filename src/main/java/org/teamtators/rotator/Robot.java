6508b174b0fa329a1fcc06943a6f4b9697479fe0
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-06-16 20:24:34 -0600

    Add SLF4J as a dependency

--- src/main/java/org/teamtators/rotator/Robot.java
+++ src/main/java/org/teamtators/rotator/Robot.java
@@ -1,11 +1,14 @@
 package org.teamtators.rotator;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The main robot class for RoTator
  */
 public class Robot extends IterativeRobot {
+    private Logger logger = LoggerFactory.getLogger(Robot.class);
 
     @Override
     public void robotInit() {
