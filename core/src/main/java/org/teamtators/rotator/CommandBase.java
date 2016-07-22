77cb86245c326865e03b0749e3d3e88288ae3d8d
John Chen <jjchen2019@gmail.com>
2016-07-21 19:51:15 -0600

    Moved CommandBase and made it extend out own command class.

---
+++ core/src/main/java/org/teamtators/rotator/CommandBase.java
@@ -0,0 +1,31 @@
+package org.teamtators.rotator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.teamtators.rotator.subsystems.scheduler.Command;
+
+/**
+ * All commands inherit from this
+ */
+public abstract class CommandBase extends Command {
+    private Logger logger = LogManager.getLogger(this.getClass());
+
+    public CommandBase(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void initialize() {
+        logger.info("Command initializing");
+    }
+
+    @Override
+    protected void finish(boolean interrupted) {
+        if (interrupted) {
+            logger.info("Command interrupted");
+        } else {
+            logger.info("Command ending");
+        }
+    }
+
+}
