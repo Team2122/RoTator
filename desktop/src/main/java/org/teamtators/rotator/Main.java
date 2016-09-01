582c1c9956505f57aa00ddcd5e38b6669933c69a
Alex Mikhalev <alexmikhalevalex@gmail.com>
2016-08-31 23:42:51 -0600

    Added new scheduler state support in desktop

--- desktop/src/main/java/org/teamtators/rotator/Main.java
+++ desktop/src/main/java/org/teamtators/rotator/Main.java
@@ -1,7 +1,7 @@
 package org.teamtators.rotator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.slf4j.Logger;
@@ -10,23 +10,37 @@ import org.teamtators.rotator.config.ConfigCommandStore;
 import org.teamtators.rotator.config.ConfigLoader;
 import org.teamtators.rotator.config.Configurables;
 import org.teamtators.rotator.operatorInterface.LogitechF310;
+import org.teamtators.rotator.scheduler.Commands;
+import org.teamtators.rotator.scheduler.RobotState;
 import org.teamtators.rotator.scheduler.Scheduler;
 import org.teamtators.rotator.subsystems.SimulationDrive;
 import org.teamtators.rotator.ui.SimulationFrame;
+import org.teamtators.rotator.ui.WASDJoystick;
 
-import javax.swing.*;
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
-import org.teamtators.rotator.scheduler.*;
-import org.teamtators.rotator.ui.WASDJoystick;
 
 public class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
-    private final YAMLMapper yamlMapper = new YAMLMapper();
-    private JFrame window;
+    @Inject
+    private ObjectMapper objectMapper;
+    @Inject
+    private SimulationFrame simulationFrame;
+    @Inject
+    private Scheduler scheduler;
+    @Inject
+    private ConfigLoader configLoader;
+    @Inject
+    private ConfigCommandStore commandStore;
+    @Inject
+    private SimulationDrive drive;
+    @Inject
+    private WASDJoystick joystick;
+
     private List<Steppable> steppables = new ArrayList<>();
     private boolean running;
-    private Scheduler scheduler;
+    private Thread thread;
 
     public static void main(String[] args) {
         try {
@@ -37,47 +51,52 @@ public class Main {
     }
 
     public void start() {
-        logger.info("Starting ui");
+        logger.info("Starting simulation robot");
 
         Injector injector = Guice.createInjector(new DesktopModule());
+        injector.injectMembers(this);
 
-        ConfigLoader configLoader = injector.getInstance(ConfigLoader.class);
-        ConfigCommandStore commandStore = injector.getInstance(ConfigCommandStore.class);
-        SimulationDrive drive = injector.getInstance(SimulationDrive.class);
-        WASDJoystick joystick = injector.getInstance(WASDJoystick.class);
-        scheduler = injector.getInstance(Scheduler.class);
 
-        scheduler.onTrigger(joystick.getTriggerSource(LogitechF310.Button.A))
-                .start(Command.log("Button A pressed"))
-                .whenPressed()
-                .start(Command.log("Button A released"))
-                .whenReleased();
-
+        logger.debug("Loading configs");
         ObjectNode commandsConfig = (ObjectNode) configLoader.load("commands.yml");
         ObjectNode simulationConfig = (ObjectNode) configLoader.load("simulation.yml");
 
+        logger.debug("Configuring subsystems");
+        Configurables.configureObject(drive, simulationConfig.get("SimulationDrive"), objectMapper);
+
+        logger.debug("Creating commands from config");
         commandStore.createCommandsFromConfig(commandsConfig);
 
-        Configurables.configureObject(drive, simulationConfig.get("SimulationDrive"), yamlMapper);
+        logger.debug("Binding triggers");
+        scheduler.onTrigger(joystick.getTriggerSource(LogitechF310.Button.A))
+                .start(Commands.log("Button A pressed"))
+                .whenPressed()
+                .start(Commands.log("Button A released"))
+                .whenReleased();
 
         steppables.add(drive);
 
-        window = injector.getInstance(SimulationFrame.class);
-        window.setVisible(true);
+        logger.info("Opening window");
+        simulationFrame.setVisible(true);
 
-        scheduler.startCommand(commandStore.getCommand("DriveTank"));
+        scheduler.enterState(RobotState.TELEOP);
+        scheduler.registerDefaultCommand(commandStore.getCommand("DriveTank"));
 
         running = true;
-        Thread thread = new Thread(this::run);
-        thread.setUncaughtExceptionHandler((t, e) ->
-                logger.error("Uncaught exception in thread {}", t, e)
+        thread = new Thread(this::run);
+        thread.setUncaughtExceptionHandler((t, e) -> {
+                    logger.error("Uncaught exception in thread {}", t, e);
+                    System.exit(-1);
+                }
         );
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            logger.info("Stopping run loop");
-            running = false;
-            thread.interrupt();
-        }));
+        Runtime.getRuntime().addShutdownHook(new Thread(this::stop));
         thread.start();
+    }
+
+    private void stop() {
+        logger.info("Stopping run loop");
+        running = false;
+        thread.interrupt();
     }
 
     public void run() {
@@ -90,11 +109,11 @@ public class Main {
             for (Steppable steppable : steppables) {
                 steppable.step(periodS);
             }
-            window.repaint();
+            simulationFrame.repaint();
             long nanoTime = System.nanoTime();
             long elapsed = (nanoTime - lastRun);
             int elapsedMS = (int) (elapsed / 1000000);
-            logger.trace("Elapsed {} ms, max {} ms", elapsedMS, periodMS);
+//            logger.trace("Elapsed {} ms, max {} ms", elapsedMS, periodMS);
             if (elapsedMS >= periodMS) {
                 logger.debug("{} ms elapsed, greater than period of {} ms", elapsedMS, periodMS);
                 continue;
