9f74d83ea6a39c9c38f3dbed95aa6dc66f41c1be
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-09-06 19:25:10 -0600

    Autoformat

--- rio/src/main/java/org/teamtators/rotator/tester/components/DigitalSensorTest.java
+++ rio/src/main/java/org/teamtators/rotator/tester/components/DigitalSensorTest.java
@@ -1,11 +1,10 @@
 package org.teamtators.rotator.tester.components;
 
 import org.teamtators.rotator.components.DigitalSensor;
-import org.teamtators.rotator.components.SensorType;
 import org.teamtators.rotator.operatorInterface.LogitechF310;
 import org.teamtators.rotator.tester.ComponentTest;
 
-public class DigitalSensorTest extends ComponentTest{
+public class DigitalSensorTest extends ComponentTest {
 
     private DigitalSensor digitalSensor;
 
@@ -21,7 +20,7 @@ public class DigitalSensorTest extends ComponentTest{
 
     @Override
     public void onButtonDown(LogitechF310.Button button) {
-        if(button == LogitechF310.Button.A) {
+        if (button == LogitechF310.Button.A) {
             logger.info("Digital sensor value {} (type {})", digitalSensor.get(), digitalSensor.getType())
         }
     }
