323e49727ac33757d8271fb4992c2c5f71a06da2
Tony Brar <tonycbrar@gmail.com>
2016-09-10 15:31:53 -0600

    Allow configuring triggers for both joysticks

--- core/src/main/java/org/teamtators/rotator/config/TriggerBinder.java
+++ core/src/main/java/org/teamtators/rotator/config/TriggerBinder.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.teamtators.rotator.operatorInterface.AbstractOperatorInterface;
 import org.teamtators.rotator.operatorInterface.LogitechF310;
 import org.teamtators.rotator.scheduler.Command;
 import org.teamtators.rotator.scheduler.CommandStore;
@@ -16,10 +17,11 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class TriggerBinder {
-    Scheduler scheduler;
-    CommandStore commandStore;
-    ObjectMapper objectMapper;
-    private final Logger logger = LoggerFactory.getLogger(TriggerBinder.class);
+    private Scheduler scheduler;
+    private CommandStore commandStore;
+    private ObjectMapper objectMapper;
+    private AbstractOperatorInterface operatorInterface;
+    private static final Logger logger = LoggerFactory.getLogger(TriggerBinder.class);
 
     @Inject
     public void setScheduler(Scheduler scheduler) {
@@ -36,30 +38,43 @@ public class TriggerBinder {
         this.objectMapper = objectMapper;
     }
 
+    @Inject
+    public void setOperatorInterface(AbstractOperatorInterface operatorInterface) {
+        this.operatorInterface = operatorInterface;
+    }
+
     /**
-     * Bind buttons to a joystick using a triggers config node
+     * Bind buttons to all joysticks using a TriggersConfig
      *
-     * @param config   Root config node of triggers
-     * @param joystick Jostick to bind buttons to
+     * @param triggersConfig Triggers configuration object
      */
-    public void bindButtonsToLogitechF310(JsonNode config, LogitechF310 joystick) {
+    public void bindButtonsToJoysticks(TriggersConfig triggersConfig) {
+        bindButtonsToLogitechF310(triggersConfig.getDriver(), operatorInterface.driverJoystick());
+    }
+
+    /**
+     * Bind buttons to all joysticks using a config node
+     *
+     * @param config Root config node of triggers
+     */
+    public void bindButtonsToJoysticks(JsonNode config) {
         TriggersConfig triggersConfig;
         try {
             triggersConfig = objectMapper.treeToValue(config, TriggersConfig.class);
         } catch (JsonProcessingException e) {
             throw new ConfigException("Failed to process triggers");
         }
-        bindButtonsToLogitechF310(triggersConfig, joystick);
+        bindButtonsToJoysticks(triggersConfig);
     }
 
     /**
      * Bind buttons to a joystick using a TriggersConfig
      *
-     * @param triggersConfig Triggers configuration
-     * @param joystick       Joystick to bind buttons to
+     * @param buttonsMap Map of buttons and their bindings
+     * @param joystick   Joystick to bind buttons to
      */
-    public void bindButtonsToLogitechF310(TriggersConfig triggersConfig, LogitechF310 joystick) {
-        Iterator<Map.Entry<LogitechF310.Button, String>> it = triggersConfig.getDriver().entrySet().iterator();
+    public void bindButtonsToLogitechF310(Map<LogitechF310.Button, String> buttonsMap, LogitechF310 joystick) {
+        Iterator<Map.Entry<LogitechF310.Button, String>> it = buttonsMap.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry line = it.next();
             TriggerAdder onTrigger = scheduler.onTrigger(joystick.getTriggerSource((LogitechF310.Button) line.getKey()));
