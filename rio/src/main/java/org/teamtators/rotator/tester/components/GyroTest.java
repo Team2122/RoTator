b8162321a52a9b235c022d0314b0145666f33a57
Tony Brar <tonycbrar@gmail.com>
2016-09-10 14:54:54 -0600

    Improve GyroTest
    Autoformat and use IGyro instead of specific implementation

--- rio/src/main/java/org/teamtators/rotator/tester/components/GyroTest.java
+++ rio/src/main/java/org/teamtators/rotator/tester/components/GyroTest.java
@@ -1,14 +1,14 @@
 package org.teamtators.rotator.tester.components;
 
-import org.teamtators.rotator.ADXRS453;
+import org.teamtators.rotator.IGyro;
 import org.teamtators.rotator.operatorInterface.LogitechF310;
 import org.teamtators.rotator.tester.ComponentTest;
 
 public class GyroTest extends ComponentTest {
 
-    private ADXRS453 gyro;
+    private IGyro gyro;
 
-    public GyroTest(String name, ADXRS453 gyro) {
+    public GyroTest(String name, IGyro gyro) {
         super(name);
         this.gyro = gyro;
     }
@@ -23,10 +23,10 @@ public class GyroTest extends ComponentTest {
     public void onButtonDown(LogitechF310.Button button) {
         if (button == LogitechF310.Button.A) {
             logger.info("Gyro Angle: {}; Rate: {}; Calibration Offset: {}", gyro.getAngle(), gyro.getRate(), gyro.getCalibrationOffset());
-        } else if(button == LogitechF310.Button.B) {
+        } else if (button == LogitechF310.Button.B) {
             logger.info("Starting gyro calibration");
             gyro.startCalibration();
-        } else if(button == LogitechF310.Button.Y) {
+        } else if (button == LogitechF310.Button.Y) {
             logger.info("Finishing gyro calibration");
             gyro.calibrate();
         }
