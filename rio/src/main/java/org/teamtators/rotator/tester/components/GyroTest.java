44928536abd583d335c6c418feb48b5f605fc564
Abby Chen <chen1049429066@gmail.com>
2016-09-10 14:40:41 -0600

    added the gyro test

---
+++ rio/src/main/java/org/teamtators/rotator/tester/components/GyroTest.java
@@ -0,0 +1,34 @@
+package org.teamtators.rotator.tester.components;
+
+import org.teamtators.rotator.ADXRS453;
+import org.teamtators.rotator.operatorInterface.LogitechF310;
+import org.teamtators.rotator.tester.ComponentTest;
+
+public class GyroTest extends ComponentTest {
+
+    private ADXRS453 gyro;
+
+    public GyroTest(String name, ADXRS453 gyro) {
+        super(name);
+        this.gyro = gyro;
+    }
+
+    @Override
+    public void start() {
+        logger.info("Press A to get the rate, the angle, and the calibration offset of the gyro");
+        logger.info("B to start calibration, and Y to end calibration");
+    }
+
+    @Override
+    public void onButtonDown(LogitechF310.Button button) {
+        if (button == LogitechF310.Button.A) {
+            logger.info("Gyro Angle: {}; Rate: {}; Calibration Offset: {}", gyro.getAngle(), gyro.getRate(), gyro.getCalibrationOffset());
+        } else if(button == LogitechF310.Button.B) {
+            logger.info("Starting gyro calibration");
+            gyro.startCalibration();
+        } else if(button == LogitechF310.Button.Y) {
+            logger.info("Finishing gyro calibration");
+            gyro.calibrate();
+        }
+    }
+}
