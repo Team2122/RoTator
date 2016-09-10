8ec4a817aaccf86a658d2a27529a892647b49482
Sentient Spud <sentientspud@hotmail.com>
2016-09-10 16:06:00 -0600

    Made gradle deploying work

--- buildSrc/src/main/groovy/org/teamtators/gradle/RioPlugin.groovy
+++ buildSrc/src/main/groovy/org/teamtators/gradle/RioPlugin.groovy
@@ -1,6 +1,7 @@
 package org.teamtators.gradle
 
 import groovy.util.logging.Slf4j
+import org.apache.log4j.LogManager
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.internal.impldep.org.mortbay.jetty.servlet.SessionHandler
@@ -8,10 +9,12 @@ import org.hidetake.groovy.ssh.Ssh
 import org.hidetake.groovy.ssh.core.Remote
 import org.hidetake.groovy.ssh.session.BadExitStatusException
 
-@Slf4j
 class RioPlugin implements Plugin<Project> {
+    static final def logger = LogManager.getLogger(RioPlugin.class)
     static final def remoteJavaPath = "/usr/local/frc/JRE/bin/java"
     static final def remoteJarPath = "/home/lvuser/FRCUserProgram.jar"
+    static final def remoteConfigPath = "/home/lvuser/"
+    static final def remoteCleanFiles = "/home/lvuser/config /home/lvuser/FRCUserProgram.jar"
     final def ssh = Ssh.newService()
 
     def Project project
@@ -22,15 +25,38 @@ class RioPlugin implements Plugin<Project> {
         project.apply plugin: 'org.hidetake.ssh'
         project.extensions.create("rio", RioPluginExtension)
 
-        project.task('javaCheck', group: 'rio',
-                description: 'Checks that the robot has the JRE installed at the correct location') << {
-            javaCheck()
+        project.task('rioClean', group: 'rio',
+                description: 'Cleans deployed files from the robot') << {
+            rioClean()
         }
 
-        project.task('deploy', group: 'rio', dependsOn: ':rio:jar',
-                description: 'Deploys the compiled code in a JAR file to the robot') << {
-            deploy()
+        project.task('rioJavaCheck', group: 'rio',
+                description: 'Checks that the robot has the JRE installed at the correct location') << {
+            rioJavaCheck()
         }
+
+        project.task('deployJar', group: 'rio', dependsOn: project.jar,
+                description: 'Deploys the compiled code in a JAR file to the robot') << {
+            deployJar()
+        }
+
+        project.task('deployConfig', group: 'rio',
+                description: 'Deploys configs to the robot') << {
+            deployConfig()
+        }
+
+        project.task('rioReboot', group: 'rio',
+                description: 'Reboots the roboRIO') << {
+            rioReboot()
+        }
+
+        project.task('rioRestart', group: 'rio',
+                description: 'Restarts the robot code on the roboRIO') << {
+            rioRestart()
+        }
+
+        project.task('deploy', group: 'rio', dependsOn: ['deployConfig', 'deployJar', 'rioRestart', 'rioJavaCheck'],
+                description: 'Deploys configs, robot code and restarts the robot code on the roboRIO')
     }
 
     private int getTeamNumber() {
@@ -71,28 +97,59 @@ class RioPlugin implements Plugin<Project> {
 
     private void inSession(@DelegatesTo(SessionHandler) Closure closure) {
         ssh.run {
+            println("Connecting to rio remote ${remote}")
             session(remote, closure)
         }
     }
 
-    void javaCheck() {
+    void rioClean() {
         inSession {
-                logger.debug("Checking for a valid JRE on the roboRIO")
-                try {
-                    execute "${remoteJavaPath} -version"
-                } catch (BadExitStatusException e) {
-                    logger.warn("There is no valid Java Runtime Environment installed on the roboRIO", e)
-                }
+            println("Cleaning deployed robot files")
+            execute "rm -rf ${remoteCleanFiles}"
+        }
+    }
+
+    void rioJavaCheck() {
+        inSession {
+            try {
+                execute "${remoteJavaPath} -version > /dev/null"
+            } catch (BadExitStatusException e) {
+                logger.warn("There is no valid Java Runtime Environment installed on the roboRIO", e)
             }
         }
     }
 
-    void deploy() {
-        ssh.run {
-            session(remote) {
-                println("Sending jar file to ${remoteJarPath}")
-                put from: jar.archivePath, into: remoteJarPath
-            }
+    void deployJar() {
+        inSession {
+            println("Deploying JAR file to ${remoteJarPath}")
+            put from: project.jar.archivePath, into: remoteJarPath
+            execute "sync"
+            println("Deploying JAR successfully")
+        }
+    }
+
+    void deployConfig() {
+        inSession {
+            println("Deploying config to ${remoteConfigPath}")
+            put from: project.getRootProject().file('./config'), into: remoteConfigPath
+            execute "sync"
+            println("Deploying config successfully")
+        }
+    }
+
+    void rioReboot() {
+        inSession {
+            println("Rebooting roboRIO")
+            execute "/sbin/reboot"
+            println("RoboRIO is rebooting")
+        }
+    }
+
+    void rioRestart() {
+        inSession {
+            println("Restarting code on roboRIO")
+            execute "killall -q netconsole-host || :"
+            println("Robot code is restarting")
         }
     }
 }
