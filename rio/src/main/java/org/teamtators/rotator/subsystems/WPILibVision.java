995e44dc7e54e5650cf87257cee911ddf33612b2
Alex Mikhalev <alexmikhalevalex@gmail.com>
2016-09-14 16:45:54 -0600

    Fixed vision methods and added vision test

--- rio/src/main/java/org/teamtators/rotator/subsystems/WPILibVision.java
+++ rio/src/main/java/org/teamtators/rotator/subsystems/WPILibVision.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import org.teamtators.rotator.config.Configurable;
 import org.teamtators.rotator.config.VictorSPConfig;
+import org.teamtators.rotator.operatorInterface.LogitechF310;
 import org.teamtators.rotator.tester.ComponentTest;
 import org.teamtators.rotator.tester.ComponentTestGroup;
 import org.teamtators.rotator.tester.ITestable;
@@ -12,8 +13,6 @@ import org.teamtators.rotator.tester.components.VictorSPTest;
 import javax.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.lang.Double.NaN;
 
 @Singleton
 public class WPILibVision extends AbstractVision implements Configurable<WPILibVision.Config>, ITestable {
@@ -25,8 +24,6 @@ public class WPILibVision extends AbstractVision implements Configurable<WPILibV
 
     private VictorSP ledPower;
     private NetworkTable table;
-    private double lastDistance;
-    private double lastAngle;
 
     @Override
     public void configure(WPILibVision.Config config) {
@@ -46,21 +43,40 @@ public class WPILibVision extends AbstractVision implements Configurable<WPILibV
 
     @Override
     public double getDistance() {
-        double distance = table.getNumber("distance", lastDistance);
-        lastDistance = distance;
-        return distance;
+        return table.getNumber("distance", Double.NaN);
     }
 
     @Override
     public double getAngle() {
-        double angle = table.getNumber("angle", NaN);
-        return angle;
+        return table.getNumber("angle", Double.NaN);
     }
 
     @Override
     public ComponentTestGroup getTestGroup() {
-        List<ComponentTest> l = new ArrayList<>();
-        l.add(new VictorSPTest("ledPower", ledPower));
-        return new ComponentTestGroup("Vision", l);
+        return new ComponentTestGroup("Vision",
+                new VictorSPTest("ledPower", ledPower),
+                new VisionTest());
+    }
+
+    private class VisionTest extends ComponentTest {
+        public VisionTest() {
+            super("visionTest");
+        }
+
+        @Override
+        public void start() {
+            logger.info("Press A to get current vision information");
+        }
+
+        @Override
+        public void onButtonDown(LogitechF310.Button button) {
+            switch (button) {
+                case A:
+                    double angle = getAngle();
+                    double distance = getDistance();
+                    logger.info("Angle = {} degrees, Distance = {} inches", angle, distance);
+                    break;
+            }
+        }
     }
 }
