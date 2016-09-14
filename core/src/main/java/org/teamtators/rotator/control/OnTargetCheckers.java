ff89343ebafa696381a9f53b5a1ff69a2258f665
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-09-13 20:58:39 -0600

    Add two new target checkers

--- core/src/main/java/org/teamtators/rotator/control/OnTargetCheckers.java
+++ core/src/main/java/org/teamtators/rotator/control/OnTargetCheckers.java
@@ -12,4 +12,34 @@ public class OnTargetCheckers {
     public static OnTargetChecker neverOnTarget() {
         return staticValue(false);
     }
+
+    public static OnTargetChecker withinError(double threshold) {
+        return (delta, controller) -> controller.getError() < threshold;
+    }
+
+    public static class SampleTime implements OnTargetChecker {
+
+        private double currentTime;
+        private double setTime;
+        private OnTargetChecker baseChecker;
+
+        public SampleTime(double setTime, OnTargetChecker baseChecker) {
+            this.setTime = setTime;
+            this.baseChecker = baseChecker;
+        }
+
+        @Override
+        public boolean compute(double delta, AbstractController controller) {
+            if(baseChecker.compute(delta, controller)) {
+                currentTime += delta;
+                if(currentTime > setTime) {
+                    return true;
+                }
+            }
+            else {
+                currentTime = 0;
+            }
+            return false;
+        }
+    }
 }
