3939453e62092193c3c01a667327971d6eb92977
Alex Mikhalev <alexmikhalevalex@gmail.com>
2016-09-15 09:59:27 -0600

    Added NoopVision class

---
+++ core/src/main/java/org/teamtators/rotator/subsystems/noop/NoopVision.java
@@ -0,0 +1,20 @@
+package org.teamtators.rotator.subsystems.noop;
+
+import org.teamtators.rotator.subsystems.AbstractVision;
+
+public class NoopVision extends AbstractVision {
+    @Override
+    public void setLEDPower(double power) {
+        
+    }
+
+    @Override
+    public double getDistance() {
+        return 0;
+    }
+
+    @Override
+    public double getAngle() {
+        return 0;
+    }
+}
