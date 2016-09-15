da29cc0083cdf0736b9cf357a5616f2af85b028e
Alex Mikhalev <alexmikhalevalex@gmail.com>
2016-09-15 10:08:54 -0600

    Organized CoreCommands by subsystem

--- core/src/main/java/org/teamtators/rotator/commands/CoreCommands.java
+++ core/src/main/java/org/teamtators/rotator/commands/CoreCommands.java
@@ -5,14 +5,17 @@ import org.teamtators.rotator.config.ConfigCommandStore;
 public class CoreCommands {
     public static void register(ConfigCommandStore commandStore) {
         commandStore.registerClass(DriveTank.class);
-        commandStore.registerClass(LogCommand.class);
-        commandStore.registerClass(PickerSetPosition.class);
+
         commandStore.registerClass(HoodSetPosition.class);
+
+        commandStore.registerClass(LogCommand.class);
+
+        commandStore.registerClass(PickerPick.class);
+        commandStore.registerClass(PickerSetPosition.class);
+
         commandStore.registerClass(TurretHome.class);
         commandStore.registerClass(TurretSetWheelSpeed.class);
-        commandStore.registerClass(TurretTarget.class);
-        commandStore.registerClass(PickerPick.class);
-        commandStore.registerClass(TurretSetWheelSpeed.class);
         commandStore.registerClass(TurretShoot.class);
+        commandStore.registerClass(TurretTarget.class);
     }
 }
