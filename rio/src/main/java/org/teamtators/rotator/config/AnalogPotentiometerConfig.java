eceb82e6af3d8b507092f138836ca433ebed4564
Sentient Spud <sentientspud@hotmail.com>
2016-10-08 13:55:47 -0600

    Fixed ball compression sensor

--- rio/src/main/java/org/teamtators/rotator/config/AnalogPotentiometerConfig.java
+++ rio/src/main/java/org/teamtators/rotator/config/AnalogPotentiometerConfig.java
@@ -4,8 +4,8 @@ import org.teamtators.rotator.components.AnalogPotentiometer;
 
 public class AnalogPotentiometerConfig {
     public int channel;
-    public double scale;
-    public double offset;
+    public double scale = 1.0;
+    public double offset = 0.0;
 
     public AnalogPotentiometer create() {
         return new AnalogPotentiometer(channel, scale, offset);
